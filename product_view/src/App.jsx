a4af8800342fb86d4999ed4d35fe8959cf5973ff
mintesnot afework <mintesnotafework12@gmail.com>
2024-06-30 13:18:25 +0300

    The end of the product loader application or component

---
+++ product_view/src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import ProductView from './product_view/index.jsx'
+function App() {
+  return (<ProductView/>);
+}
+
+export default App
